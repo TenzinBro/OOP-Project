ea73809937a5bee2aa58f6fdea40d615a6b0619f
TenzinBro <151788344+TenzinBro@users.noreply.github.com>
2023-11-24 08:46:28 -0500

    Update FlightManager.cs

--- FlightManager.cs
+++ FlightManager.cs
@@ -4,67 +4,66 @@
         private int numPassengers;
         private Flight[] flightList;
 
-        public FlightManager(int max) 
-        {
-            numPassengers = 0;
-            this.maxSeats = max;
-            flightList = new Flight[max]
-        }
+       public FlightManager(int max)
+      {
+           numPassengers = 0;
+           this.maxSeats = max;
+           flightList = new Flight[max];
+      }
 
-        public bool addFlight(int flightNumber, string origin, string destination)
-        {
-            if(numPassengers<maxSeats)
-            {
-                flightList[numPassengers] = new Flight(flightNumber, origin, destination);
-                numPassengers++;
-                return true;
-            }
-            return false;
-        }
+public bool addFlight(int flightNumber, string origin, string destination)
+{
+    if (numPassengers < maxSeats)
+    {
+        flightList[numPassengers] = new Flight(flightNumber, origin, destination);
+        numPassengers++;
+        return true;
+    }
+    return false;
+}
 
-        public bool deleteFlight(int flightNumber, string origin, string destination)
+public bool deleteFlight(int flightNumber)
+{
+    int index = Search(flightNumber);
+    if (index != -1)
+    {
+        for (int i = index; i < numPassengers - 1; i++)
         {
-            int index = Search(flightNumber);
-            if (index != -1)
-            {
-              for (int i = index; i < numPassengers - 1; i++)
-              {
-                flightList[i] = flightList[i + 1];
-              }
-              numPassengers--;
-              return true;
-           }
-           return false;
+            flightList[i] = flightList[i + 1];
         }
+        numPassengers--;
+        return true;
+    }
+    return false;
+}
 
-        private int Search(int flightNumber)
+private int Search(int flightNumber)
+{
+    for (int x = 0; x < numPassengers; x++)
+    {
+        if (flightList[x].getFlightNum() == flightNumber)
         {
-            for (int x=0;x<numPassengers;x++)
-            {
-                if (flightListList[x].getFlightNum() == flightNumber)
-                {
-                    return x;
-                }
-            }
-            return -1;
+            return x;
         }
+    }
+    return -1;
+}
 
-        public Flight findFlight(int flightNumber)
-        {
-            int find = Search(flightNumber);
-            if (find != -1)
-            {
-                return flightList[find];
-            }
-            return null;
-        }
+public Flight findFlight(int flightNumber)
+{
+    int find = Search(flightNumber);
+    if (find != -1)
+    {
+        return flightList[find];
+    }
+    return null;
+}
 
-        public void printAllFlights()
-        {
-            string s = "--- Flight List ---";
-            for (int x = 0; x < numPassengers; x++)
-            {
-                Console.WriteLine(flightList[x].ToString());
-            }
-        }
+public void printAllFlights()
+{
+    for (int x = 0; x < numPassengers; x++)
+    {
+        Console.WriteLine(flightList[x].ToString());
+    }
+}
     }
