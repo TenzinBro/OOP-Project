7cc5b10cff6e3cdb858d17509958e7bba8d92b15
TenzinBro <151788344+TenzinBro@users.noreply.github.com>
2023-11-26 01:04:02 -0500

    Update Flight.cs

--- Flight.cs
+++ Flight.cs
@@ -1,27 +1,25 @@
-public class Flight
+internal class Flight
+{
+    public int flightNumber;
+    public string origin;
+    public string destination;
+
+    public Flight(int flightNumber, string origin, string destination)
     {
-        public int flightNumber;
-        public string origin;
-        public string destination;
+        this.flightNumber = flightNumber;
+        this.origin = origin;
+        this.destination = destination;
+    }
 
-        public Flight(int flightNumber, string origin, string destination)
-        {
-            this.flightNumber = flightNumber;
-            this.origin = origin;
-            this.destination = destination;
-        }
+    public int getFlightNum() { return flightNumber; }
+    public string getOrigin() { return origin; }
+    public string getDestination() { return destination; }
 
-        public int getFlightNum() {return flightNumber;}
-        public string getOrigin() {return origin;}
-        public string getDestination() {return destination;}
-
-        public override string ToString()
-        {
-            string s = "Flight Number: " + flightNumber;
-            s = s + "\nOrigin: " + origin;
-            s = s + "\nDestination: " + destination;
-            return s;
-        }
-
-    } 
+    public override string ToString()
+    {
+        string s = "Flight Number: " + flightNumber;
+        s = s + "\nOrigin: " + origin;
+        s = s + "\nDestination: " + destination;
+        return s;
+    }
 }
