d1fcb84de6d5f0c7bb12e156dc145e90107445dc
TenzinBro <151788344+TenzinBro@users.noreply.github.com>
2023-11-26 01:07:35 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,110 +1,46 @@
- public class Program
- {
-      static FlightManager flightManager;
-
-     static void addFlightProcess()
-     {
-         int flightNumber;
-         string origin, destination;
-         Console.Clear();
-         Console.WriteLine("Enter the Flight Number: ");
-         flightNumber = Convert.ToInt32(Console.ReadLine());
-         Console.WriteLine("Enter the Origin of the Flight: ");
-         origin = Console.ReadLine();
-         Console.WriteLine("Enter the Destination of the Flight: ");
-         destination = Console.ReadLine();
-
-         bool success = flightManager.addFlight(flightNumber, origin, destination);
-         if (success)
-         {
-             Console.WriteLine("Flight added successfully!");
-         }
-         else
-         {
-             Console.WriteLine("Failed to add flight. Maximum capacity reached.");
-         }
-         Console.ReadLine();
-     }
-
-     static void viewFlightsProcess()
-     {
-         Console.Clear();
-         Console.WriteLine("*** Flight List ***");
-         flightManager.printAllFlights();
-         Console.ReadLine();
-     }
-
-     static void viewSpecificFlightProcess()
-     {
-         Console.Clear();
-         Console.WriteLine("Enter the Flight Number: ");
-         int flightNumber = Convert.ToInt32(Console.ReadLine());
-         Flight flight = flightManager.findFlight(flightNumber);
-         if (flight != null)
-         {
-             Console.WriteLine(flight.ToString());
-         }
-         else
-         {
-             Console.WriteLine("Flight not found.");
-         }
-         Console.ReadLine();
-     }
-
-     static void deleteFlightProcess()
-     {
-         Console.Clear();
-         Console.WriteLine("Enter the Flight Number: ");
-         int flightNumber = Convert.ToInt32(Console.ReadLine());
-         bool success = flightManager.deleteFlight(flightNumber);
-         if (success)
-         {
-             Console.WriteLine("Flight deleted successfully!");
-         }
-         else
-         {
-             Console.WriteLine("Failed to delete flight. Flight not found.");
-         }
-         Console.ReadLine();
-     }
-
-     static void flightMenu()
-     {
-         Console.Clear();
-         Console.WriteLine("*** Flight List ***");
-         Console.WriteLine(" 1. Add Flight");
-         Console.WriteLine(" 2. View Flights");
-         Console.WriteLine(" 3. View a specific flight");
-         Console.WriteLine(" 4. Delete Flight");
-         Console.WriteLine(" 5. Exit");
-     }
+public class Program
+{
+    static FlightManager flightManager = new FlightManager(10000); // You can set the desired maxSeats value
 
      static void Main(string[] args)
      {
-         flightManager = new FlightManager(10);
-         int choice = 0;
-         while (choice != 5)
+         RunFlightMenu();
+     }
+
+     static void RunFlightMenu()
+     {
+         while (true)
          {
-             flightMenu();
+             Console.Clear();
+             Console.WriteLine("*==XYZ Airlines Limited==*\n");
+
+             Console.WriteLine("FLIGHT'S MENU");
+             Console.WriteLine("Please select a choice from the menu below:");
+             Console.WriteLine("1. Add Flight");
+             Console.WriteLine("2. View Flights");
+             Console.WriteLine("3. View a particular Flight");
+             Console.WriteLine("4. Delete Flight");
+             Console.WriteLine("5. Back to Main Menu\n");
+
              Console.WriteLine("Enter your choice: ");
-             choice = Convert.ToInt32(Console.ReadLine());
+             int choice = Convert.ToInt32(Console.ReadLine());
 
              switch (choice)
              {
                  case 1:
-                     addFlightProcess();
+                     flightManager.AddFlightProcess();
                      break;
                  case 2:
-                     viewFlightsProcess();
+                     flightManager.ViewFlightsProcess();
                      break;
                  case 3:
-                     viewSpecificFlightProcess();
+                     flightManager.ViewSpecificFlightProcess();
                      break;
                  case 4:
-                     deleteFlightProcess();
+                     flightManager.DeleteFlightProcess();
                      break;
                  case 5:
-                     Console.WriteLine("Exiting...");
+                     Environment.Exit(0);
                      break;
                  default:
                      Console.WriteLine("Invalid choice. Please try again.");
@@ -112,4 +48,4 @@
              }
          }
      }
- }
+}
